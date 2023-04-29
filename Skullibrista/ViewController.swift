eb5a0ed9246fae537d6d8e71a5aa3a13cbe2be83
Abner Lima <abnerlima.1997@gmail.com>
2023-04-29 14:45:58 -0300

    Implementando a logic do game

--- Skullibrista/ViewController.swift
+++ Skullibrista/ViewController.swift
@@ -17,7 +17,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbInstructions: UILabel!
     
     var isMoving: Bool = false
+    var gameTimer: Timer!
     lazy var motionManager = CMMotionManager()
+    var startDate: Date!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,7 +59,15 @@ class ViewController: UIViewController {
         lbInstructions.isHidden = true
         vGameOver.isHidden = true
         isMoving = false
+        player.transform = CGAffineTransform(rotationAngle: 0)
+        street.transform = CGAffineTransform(rotationAngle: 0)
+        startDate = Date()
+              
         useMotionManager()
+        
+        gameTimer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true, block: { timer in
+            self.rotateWorld()
+        })
     }
     
     func useMotionManager() {
@@ -66,6 +76,10 @@ class ViewController: UIViewController {
                 if error == nil {
                     if let data = data {
                         self.rotatePlayer(x: data.gravity.x, y: data.gravity.y)
+                        
+                        if !self.isMoving {
+                            self.checkGameOver()
+                        }
                     }
                 }
             }
@@ -76,8 +90,37 @@ class ViewController: UIViewController {
         let angle = atan2(x, y) - .pi
         player.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
     }
+    
+    func rotateWorld() {
+        let randomAngle = Double(arc4random_uniform(120)) / 100 - 0.6
+        isMoving = true
+        UIView.animate(withDuration: 0.75, animations: {
+            self.street.transform = CGAffineTransform(rotationAngle: randomAngle)
+        }) { success in
+            self.isMoving = false
+        }
+    }
+    
+    func checkGameOver() {
+        let worldAngle = atan2(street.transform.a, street.transform.b)
+        let playerAngle = atan2(player.transform.a, player.transform.b)
+        let difference = abs(worldAngle - playerAngle)
+        
+        if difference > 0.25 {
+            gameTimer.invalidate()
+            vGameOver.isHidden = false
+            motionManager.stopDeviceMotionUpdates()
+            presentSecondsPlayed()
+        }
+    }
+    
+    func presentSecondsPlayed() {
+        let secondsPlayed = round(Date().timeIntervalSince(startDate))
+        lbTimePlayed.text = "Você jogou durante \(secondsPlayed) segundos."
+    }
 
     @IBAction func playAgain(_ sender: UIButton) {
+        start()
     }
     
 }
