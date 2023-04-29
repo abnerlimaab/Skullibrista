82ae8910dab0b8a0c9f67b0e3e6949f7d6b88546
Abner Lima <abnerlima.1997@gmail.com>
2023-04-29 11:10:01 -0300

    CoreMotion: usando acelerômetro e giroscopio

--- Skullibrista/ViewController.swift
+++ Skullibrista/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
     
@@ -14,10 +15,66 @@ class ViewController: UIViewController {
     @IBOutlet weak var vGameOver: UIView!
     @IBOutlet weak var lbTimePlayed: UILabel!
     @IBOutlet weak var lbInstructions: UILabel!
+    
+    var isMoving: Bool = false
+    lazy var motionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         vGameOver.isHidden = true
+        prepareStreet()
+        preparePlayer()
+        animatePlayer()
+        
+        Timer.scheduledTimer(withTimeInterval: 6.0, repeats: false) { timer in
+            self.start()
+        }
+    }
+    
+    func prepareStreet() {
+        street.frame.size.width = view.frame.size.width * 2
+        street.frame.size.height = view.frame.size.height * 2
+        street.center = view.center
+    }
+    
+    func preparePlayer() {
+        player.center = view.center
+    }
+    
+    func animatePlayer() {
+        player.animationImages = []
+        
+        for i in 0...7 {
+            let image = UIImage(named: "player\(i)")!
+            player.animationImages?.append(image)
+        }
+        
+        player.animationDuration = 0.5
+        player.startAnimating()
+    }
+    
+    func start() {
+        lbInstructions.isHidden = true
+        vGameOver.isHidden = true
+        isMoving = false
+        useMotionManager()
+    }
+    
+    func useMotionManager() {
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { data, error in
+                if error == nil {
+                    if let data = data {
+                        self.rotatePlayer(x: data.gravity.x, y: data.gravity.y)
+                    }
+                }
+            }
+        }
+    }
+    
+    func rotatePlayer(x: Double, y: Double) {
+        let angle = atan2(x, y) - .pi
+        player.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
     }
 
     @IBAction func playAgain(_ sender: UIButton) {
